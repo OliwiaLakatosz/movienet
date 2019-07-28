d620fe8697ba4fbbae3ef8eb1acaf62402f4337f
Oliwia <olakatosz18@gmail.com>
2019-07-28 21:10:59 +0200

    add missing @Repository

--- movienet-backend/movienet/src/main/java/com/pasquda/repositories/UserRepository.java
+++ movienet-backend/movienet/src/main/java/com/pasquda/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.pasquda.repositories;
 
 import com.pasquda.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
